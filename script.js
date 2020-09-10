c39effb1c33035fd6b85ee9b2bbc01e0d2c2b598
baroman <baroman4i@mail.ru>
2020-09-10 13:12:28 +0300

    часть 3

--- script.js
+++ script.js
@@ -22,12 +22,18 @@ const score = document.querySelector('.score'),
   car.classList.add('car')
   
   
-
+  let bgRandom = 0
 start.addEventListener('click', startGame)
 
 function startGame() {
   start.classList.add('hide')
+  gameArea.innerHTML = ''
   setting.start = true
+  car.style.bottom = '10px'
+  car.style.left = '125px'
+  car.style.top = 'auto'
+  keys.ArrowLeft = false
+  keys.ArrowRight = false
   for (let i = 0; i < 20; i++) {
       const line = document.createElement('div')
       line.classList.add('line')
@@ -35,13 +41,23 @@ function startGame() {
       gameArea.appendChild(line)
   }
   for (let i = 0; i < getQuantityElements(100*setting.traffic); i++) {
+    bgRandom = Math.floor(Math.random()*2)
     const enemy = document.createElement('div')
     enemy.classList.add('enemy')
     enemy.y = -100*setting.traffic*(i+1)
     enemy.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50))+'px'
+    if (bgRandom >= 1) {
+          enemy.style.backgroundImage = 'url(image/enemy2.png)'
+        }
+        else {
+          enemy.style.backgroundImage = 'url(image/enemy.png)'
+        }
     enemy.style.top = enemy.y+'px'
+    
     gameArea.appendChild(enemy)
+    
   }
+  setting.score = 0
   gameArea.appendChild(car)
   setting.x = car.offsetLeft
   setting.y = car.offsetTop
@@ -51,6 +67,8 @@ function playGame() {
   moveRoad()
   moveEnemy()
   if (setting.start) {
+    setting.score += setting.speed
+    score.innerHTML = setting.score
     if (keys.ArrowUp && setting.y > 0) {
       setting.y -= setting.speed
     }
@@ -89,17 +107,25 @@ function moveRoad() {
   });
 
 }
+
 let topEn = 0
 function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy')
   enemy.forEach(en => {
+    let carRect = car.getBoundingClientRect()
+    let enemyRect = en.getBoundingClientRect()
+
+    if (carRect.top <= enemyRect.bottom-10 && carRect.right-10 >= enemyRect.left && carRect.left <= enemyRect.right-10 && carRect.bottom-10 >= enemyRect.top) {
+      setting.start = false
+      alert('GAME OVER. score = '+setting.score)
+      start.classList.remove('hide')
+    }
     topEn = en.offsetTop + setting.speed*1.3
     en.style.top = topEn+'px'
       if (topEn >= document.documentElement.clientHeight) {
         topEn = -75
         en.style.left = Math.floor(Math.random()*(gameArea.offsetWidth-50))+'px'
         en.style.top = topEn+'px'
-
       }
     });
     
